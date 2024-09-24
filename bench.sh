0c5be0d31f05ca3aa55f4a06632b9636ec3165c9
ttyS3 <ttys3.rust@gmail.com>
2024-09-24 15:01:34 +0000

    chore: add new benchmarking script and remove old script

---
+++ bench.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ab -n 1000 -c 100 http://localhost:6688/
+
+
--- go.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for (( ; ; ))
-do
-    curl -X GET -vvv --connect-timeout 1 --max-time 2  http://localhost:6688/
-done
-
-
