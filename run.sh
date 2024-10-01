f8713b02112c5ca7ab567e0f3101b21c11a24fa6
ttyS3 <ttys3.rust@gmail.com>
2024-10-01 02:00:51 +0000

    chore: update environment variable settings in run script

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 set -eou pipefail
 
 truncate -s 0 /tmp/a.log
-env GOGC=50 GODEBUG=gctrace=1 ./traefik --configFile=./traefik.yaml 2>&1 | tee /tmp/a.log
+#env GOGC=50 GODEBUG=gctrace=1 ./traefik --configFile=./traefik.yaml 2>&1 | tee /tmp/a.log
+env GODEBUG=gctrace=1 ./traefik --configFile=./traefik.yaml 2>&1 | tee /tmp/a.log
 
